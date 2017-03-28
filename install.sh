6d27cf6b8eddd2f3a7cd62e5b5140826c4c95ca5
Alex Nicksay <nicksay@gmail.com>
2017-03-28 15:27:09 -0400

    Convert git-new to an alias

--- install.sh
+++ install.sh
@@ -5,6 +5,11 @@ set -e  # Stop on error.
 # Run from the the script directory.
 cd "$(dirname "$0")"
 
+# Install scripts.
 mkdir -p "$HOME/bin"
-
 rsync -avh --no-perms "$PWD"/git-* "$HOME/bin/"
+
+# Insall aliases.
+if ! git config --global --get alias.new >/dev/null; then
+  git config --global alias.new "!git checkout master && git checkout -b"
+fi
