697fa37feb447a5dd1e605615c81f136cd5571ee
Alex Nicksay <nicksay@gmail.com>
2024-04-12 13:13:09 -0700

    Revert "Convert git-done to an alias"
    This reverts commit c3eccc2d3f81c3134a1ee1f526dcf02180a5b5b4.

--- install.sh
+++ install.sh
@@ -5,16 +5,11 @@ set -e  # Stop on error.
 # Run from the the script directory.
 cd "$(dirname "$0")"
 
-echo "Installing scripts..."
+# Install scripts.
 mkdir -p "$HOME/bin"
 rsync -avh --no-perms "$PWD"/git-* "$HOME/bin/"
-echo "Done."
 
-echo "Installing aliases..."
+# Insall aliases.
 if ! git config --global --get alias.new >/dev/null; then
   git config --global alias.new "!git checkout master && git checkout -b"
 fi
-if ! git config --global --get alias.done >/dev/null; then
-  git config --global alias.done "!git checkout master && git sync && git tidy"
-fi
-echo "Done."
