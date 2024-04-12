bea312eed5cd8c8829ed4cca4c2f8dc5d20fd074
Alex Nicksay <nicksay@gmail.com>
2024-04-12 13:13:18 -0700

    Revert "Convert git-new to an alias"
    This reverts commit 6d27cf6b8eddd2f3a7cd62e5b5140826c4c95ca5.

--- install.sh
+++ install.sh
@@ -5,11 +5,6 @@ set -e  # Stop on error.
 # Run from the the script directory.
 cd "$(dirname "$0")"
 
-# Install scripts.
 mkdir -p "$HOME/bin"
-rsync -avh --no-perms "$PWD"/git-* "$HOME/bin/"
 
-# Insall aliases.
-if ! git config --global --get alias.new >/dev/null; then
-  git config --global alias.new "!git checkout master && git checkout -b"
-fi
+rsync -avh --no-perms "$PWD"/git-* "$HOME/bin/"
